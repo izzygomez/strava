6d20b31dcf6f429eef1bfd05aa03eeeccb766248
Izzy Gomez <izzygmit@gmail.com>
2024-10-21 19:42:26 -0400

    minor comment & print statement changes

--- analyze_strava.py
+++ analyze_strava.py
@@ -1,6 +1,7 @@
 import os
+import time
 from collections import defaultdict
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import pytz
 from dotenv import load_dotenv
@@ -54,12 +55,13 @@ def erie_marathon_check():
     # print("Erie Marathon description:", erie_marathon_detailed["description"])
 
 
+# TODO consider adding an optional filter for different workout types, e.g. only runs
 def longest_workout_breaks(
-    start_date: datetime, end_date: datetime, additional_breaks=0
+    start_date: datetime, end_date: datetime, additional_breaks=0, activity_type=None
 ):
     """Find the longest breaks in [start_date, end_date).
 
-    Prints the longest break and, if specified, the next 'additional_breaks' longest breaks.
+    Prints the longest break &, if specified, the next 'additional_breaks' longest breaks.
     """
     # Fetch Strava activities from the specified date range
     all_activities = strava.get_strava_activities(ACCESS_TOKEN, start_date, end_date)
@@ -80,8 +82,8 @@ def longest_workout_breaks(
     # )
 
     print(
-        "Processing %d activities from Strava between %s & %s."
-        % (len(all_activities), start_date.date(), end_date.date())
+        "Processing %d activities from Strava from %s to %s (inclusive)."
+        % (len(all_activities), start_date.date(), end_date.date() - timedelta(days=1))
     )
 
     # Dictionary to store all breaks between workouts & the pair of dates
@@ -92,11 +94,11 @@ def longest_workout_breaks(
         next_activity = all_activities[i + 1]
 
         # Convert both activity timestamps to correct local timezone based on the "timezone" field
-        # The timezone field has the format '(GMT-05:00) America/New_York', so we split on the space and take the second part
+        # The timezone field has the format '(GMT-05:00) America/New_York', so we split on the space & take the second part
         activity_timezone = pytz.timezone(activity["timezone"].split(" ")[1])
         next_activity_timezone = pytz.timezone(next_activity["timezone"].split(" ")[1])
 
-        # Convert activity and next_activity to their respective timezones
+        # Convert activity & next_activity to their respective timezones
         activity_date = (
             datetime.fromisoformat(activity["start_date_local"][:-1])
             .astimezone(activity_timezone)
@@ -119,8 +121,8 @@ def longest_workout_breaks(
 
         breaks[break_time].append(
             [
-                activity_date,
-                next_activity_date,
+                activity_date + timedelta(days=1),
+                next_activity_date - timedelta(days=1),
             ]
         )
 
@@ -133,39 +135,40 @@ def longest_workout_breaks(
 
     # Print the longest break
     longest_break = sorted_break_lengths[0]
-    print("\nThe longest break between workouts is", longest_break, "days.")
+    print("\nThe longest break between workouts was", longest_break, "days.")
     if len(breaks[longest_break]) > 1:
         print(
-            "There are multiple breaks (%d) of this length. They are between:"
+            "There are multiple (%d) breaks of this length. They were from:"
             % len(breaks[longest_break])
         )
         for longest_break_dates in breaks[longest_break]:
-            print("\t- %s and %s." % tuple(longest_break_dates))
+            print("\t- %s to %s." % tuple(longest_break_dates))
     else:
         longest_break_dates = breaks[longest_break][0]
         print(
-            "This break was between %s and %s." % tuple(longest_break_dates),
+            "This break was from %s to %s." % tuple(longest_break_dates),
         )
 
     # Print additional longest breaks if requested
     for i in range(1, min(additional_breaks + 1, len(sorted_break_lengths))):
         next_longest_break = sorted_break_lengths[i]
-        print("\nThe next longest break is", next_longest_break, "days.")
+        print("\nThe next longest break was", next_longest_break, "days.")
         if len(breaks[next_longest_break]) > 1:
             print(
-                "There are multiple breaks (%d) of this length. They are between:"
+                "There are multiple (%d) breaks of this length. They were from:"
                 % len(breaks[next_longest_break])
             )
             for break_dates in breaks[next_longest_break]:
-                print("\t- %s and %s." % tuple(break_dates))
+                print("\t- %s to %s." % tuple(break_dates))
         else:
             break_dates = breaks[next_longest_break][0]
-            print("This break was between %s and %s." % tuple(break_dates))
+            print("This break was from %s to %s." % tuple(break_dates))
 
 
 if __name__ == "__main__":
     # erie_marathon_check()
 
-    start_date = datetime(2023, 1, 1)
-    end_date = datetime(2024, 10, 10)
-    longest_workout_breaks(start_date, end_date, additional_breaks=3)
+    start_date = datetime(2024, 1, 1)
+    # end_date = datetime(2024, 10, 10)
+    tomorrow = datetime.now() + timedelta(days=1)
+    longest_workout_breaks(start_date, tomorrow, additional_breaks=3)
