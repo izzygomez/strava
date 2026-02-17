4293c5829cea9890f9cf1a130c78fa4cfd4b206c
Izzy Gomez <izzy@izzygomez.com>
2026-02-17 01:29:19 -0500

    add one-time fixes for new basketball & volleyball sport types (#33)
    see [this reddit thread](https://www.reddit.com/r/Strava/comments/1r3dlab/strava_has_finally_added_padel/) & [this strava support page](https://support.strava.com/hc/en-us/articles/216919407-Supported-Sport-Types-on-Strava)

--- scripts/misc.py
+++ scripts/misc.py
@@ -1,4 +1,5 @@
 import os
+from collections import defaultdict
 from datetime import datetime
 
 from services import strava_api
@@ -16,46 +17,68 @@ ACCESS_TOKEN = strava_api.get_strava_access_token(
 
 def log_activities(activities, file_name=""):
     """
-    Helper function to log activities either to console (file_name='') or to a file. Formatting across both options is consistent. Files will be saved in the 'exports' directory at the root of the project.
+    Helper function to log activities either to console (file_name="") or to a
+    file. Formatting across both options is consistent. Files will be saved in
+    the exports/ directory at the root of the project.
     """
 
     def _log_activities(activities, file=None):
-        main_header = f"Total activities: {len(activities)}\n"
-        if file:
-            file.write(main_header)
-        else:
-            print(main_header)
+        """Render grouped activity output to either file or stdout."""
 
-        for sport_type in strava_api.SPORT_TYPE_TO_EMOJI.keys():
-            filtered_activities = [
-                activity
-                for activity in activities
-                if activity["sport_type"] == sport_type
-            ]
+        def _write_line(line=""):
+            """Write one line to the selected output target."""
+            if file:
+                file.write(f"{line}\n")
+            else:
+                print(line)
+
+        def _format_activity(activity):
+            """Format one activity as a display/export line."""
+            return (
+                f"{activity['name']} on "
+                f"{datetime.fromisoformat(activity['start_date_local']).strftime('%m/%d/%Y')}: "
+                f"{strava_api.get_activity_url(activity['id'])}"
+            )
+
+        timestamp = datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S %Z")
+        _write_line(f"Total activities: {len(activities)}")
+        _write_line(f"Export created: {timestamp}")
+
+        activities_by_sport_type = defaultdict(list)
+        for activity in activities:
+            activities_by_sport_type[activity["sport_type"]].append(activity)
+
+        mapped_sport_types = sorted(strava_api.SPORT_TYPE_TO_EMOJI.keys())
+        mapped_sport_type_set = set(mapped_sport_types)
+
+        def _render_sport_type_section(sport_type):
+            """Render one sport type section if it has activities."""
+            filtered_activities = activities_by_sport_type.get(sport_type, [])
             if not filtered_activities:
-                continue
+                return
 
-            sport_type_header = f"{sport_type} ({len(filtered_activities)} activities):"
-            if file:
-                file.write("\n" + sport_type_header)
-            else:
-                print(sport_type_header)
-
+            _write_line()
+            _write_line(f"{sport_type} ({len(filtered_activities)} activities):")
             for activity in filtered_activities:
-                activity_str = (
-                    f"{activity['name']} on "
-                    f"{datetime.fromisoformat(activity['start_date_local']).strftime('%m/%d/%Y')}: "
-                    f"{strava_api.get_activity_url(activity['id'])}"
-                )
-                if file:
-                    file.write("\n" + activity_str)
-                else:
-                    print(activity_str)
+                _write_line(_format_activity(activity))
 
-            if file:
-                file.write("\n")
-            else:
-                print()
+        for sport_type in mapped_sport_types:
+            _render_sport_type_section(sport_type)
+
+        # Sport types that exist in data but are not yet mapped to emojis in
+        # SPORT_TYPE_TO_EMOJI dict.
+        unmapped_sport_types = sorted(
+            sport_type
+            for sport_type in activities_by_sport_type.keys()
+            if sport_type not in mapped_sport_type_set
+        )
+        if not unmapped_sport_types:
+            return
+
+        _write_line()
+        _write_line("Unmapped sport types:")
+        for sport_type in unmapped_sport_types:
+            _render_sport_type_section(sport_type)
 
     if file_name:
         file_path = os.path.join(EXPORTS_DIR, file_name)
@@ -69,6 +92,7 @@ def log_activities(activities, file_name=""):
 
 
 def log_2025_activities_to_console():
+    """Export 2025-to-date activities to console for quick inspection."""
     start_date = datetime(2025, 1, 1, tzinfo=time_utils.EASTERN)
     end_date = time_utils.today()
 
@@ -81,6 +105,7 @@ def log_2025_activities_to_console():
 
 
 def write_all_activities_to_file():
+    """Write all activities to a timestamped export file."""
     start_date = time_utils.izzys_strava_start_date()
     end_date = time_utils.today()
 
@@ -88,11 +113,13 @@ def write_all_activities_to_file():
         ACCESS_TOKEN, start_date, end_date
     )
 
-    file_name = "strava-all-activities-export.txt"
+    timestamp_prefix = datetime.now().astimezone().strftime("%Y-%m-%d_%H-%M-%S")
+    file_name = f"{timestamp_prefix}_strava-all-activities-export.txt"
     log_activities(activities, file_name=file_name)
 
 
 def write_all_workout_activities_to_file():
+    """Write only Workout sport_type activities to a timestamped file."""
     start_date = time_utils.izzys_strava_start_date()
     end_date = time_utils.today()
 
@@ -101,7 +128,8 @@ def write_all_workout_activities_to_file():
         ACCESS_TOKEN, start_date, end_date, sport_type_filters=sport_type_filters
     )
 
-    file_name = "strava-workout-activities-export.txt"
+    timestamp_prefix = datetime.now().astimezone().strftime("%Y-%m-%d_%H-%M-%S")
+    file_name = f"{timestamp_prefix}_strava-workout-activities-export.txt"
     log_activities(activities, file_name=file_name)
 
 
@@ -130,13 +158,64 @@ def fix_soccer_activities():
         )
 
 
+def fix_basketball_activities():
+    """
+    Iterate through all 'Workout' activities that have basketball emoji & update
+    them to have the 'Basketball' SportType. This is a one-time fix, & should no
+    longer be necessary to run after the initial run.
+    """
+    start_date = time_utils.izzys_strava_start_date()
+    end_date = time_utils.today()
+
+    sport_type_filters = ["Workout"]
+    activities = strava_api.get_sorted_strava_activities(
+        ACCESS_TOKEN, start_date, end_date, sport_type_filters=sport_type_filters
+    )
+    basketball_activities = [
+        activity for activity in activities if "🏀" in activity["name"]
+    ]
+    print(f"Found {len(basketball_activities)} basketball activities.")
+
+    for activity in basketball_activities:
+        print()
+        strava_api.update_strava_activity(
+            ACCESS_TOKEN, activity_id=activity["id"], data={"sport_type": "Basketball"}
+        )
+
+
+def fix_volleyball_activities():
+    """
+    Iterate through all 'Workout' activities that have volleyball emoji & update
+    them to have the 'Volleyball' SportType. This is a one-time fix, & should no
+    longer be necessary to run after the initial run.
+    """
+    start_date = time_utils.izzys_strava_start_date()
+    end_date = time_utils.today()
+
+    sport_type_filters = ["Workout"]
+    activities = strava_api.get_sorted_strava_activities(
+        ACCESS_TOKEN, start_date, end_date, sport_type_filters=sport_type_filters
+    )
+    volleyball_activities = [
+        activity for activity in activities if "🏐" in activity["name"]
+    ]
+    print(f"Found {len(volleyball_activities)} volleyball activities.")
+
+    for activity in volleyball_activities:
+        print()
+        strava_api.update_strava_activity(
+            ACCESS_TOKEN, activity_id=activity["id"], data={"sport_type": "Volleyball"}
+        )
+
+
 if __name__ == "__main__":
     ### Misc functions that I'm currently toying with
-
-    log_2025_activities_to_console()
-
+    # log_2025_activities_to_console()
     # write_all_activities_to_file()
-
     # write_all_workout_activities_to_file()
 
+    ### One-time fixes
     # fix_soccer_activities()
+    # fix_basketball_activities()
+    # fix_volleyball_activities()
+    pass
